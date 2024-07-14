1532e18e37a3ae5a646b9cfc57266facc2ac2eb5
Noah Solomon <noahsolomon2003@gmail.com>
2024-07-14 11:02:39 -0400

    initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,21 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps herevim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+
+vim.keymap.set("n", "<C-w>-", "<cmd>split<CR>", { desc = "Split window below" })
+vim.keymap.set("n", "<C-w>|", "<cmd>vsplit<CR>", { desc = "Split window right" })
+vim.keymap.set("n", "<C-w>d", "<cmd>close<CR>", { desc = "Delete window" })
+vim.keymap.set("n", "<C-w>m", "<cmd>MaximizerToggle<CR>", { desc = "Maximize toggle" })
+-- Alternative window navigation using leader key
+vim.keymap.set("n", "<leader>wh", "<C-w>h", { desc = "Go to left window" })
+vim.keymap.set("n", "<leader>wj", "<C-w>j", { desc = "Go to lower window" })
+vim.keymap.set("n", "<leader>wk", "<C-w>k", { desc = "Go to upper window" })
+vim.keymap.set("n", "<leader>wl", "<C-w>l", { desc = "Go to right window" })
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
+vim.keymap.set("n", "n", "nzzzv", { noremap = true, silent = true })
+vim.keymap.set("n", "N", "Nzzzv", { noremap = true, silent = true })
---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,73 @@
+-- return {
+--   "nvim-neo-tree/neo-tree.nvim",
+--   opts = {
+--     filesystem = {
+--       filtered_items = {
+--         visible = true,
+--         show_hidden_count = true,
+--         hide_dotfiles = false,
+--         hide_gitignored = true,
+--         hide_by_name = {
+--           -- '.git',
+--           -- '.DS_Store',
+--           -- 'thumbs.db',
+--         },
+--         never_show = {},
+--       },
+--     },
+--   },
+-- }
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          -- '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+        never_show = {},
+      },
+      -- Add these options for better icon support
+      use_libuv_file_watcher = true,
+      follow_current_file = {
+        enabled = true,
+      },
+      group_empty_dirs = true,
+      window = {
+        mappings = {
+          ["<space>"] = "none",
+        },
+      },
+    },
+    default_component_configs = {
+      icon = {
+        folder_closed = "",
+        folder_open = "",
+        folder_empty = "",
+        default = "",
+      },
+      modified = {
+        symbol = "[+]",
+      },
+      git_status = {
+        symbols = {
+          added = "",
+          deleted = "",
+          modified = "",
+          renamed = "➜",
+          untracked = "★",
+          ignored = "◌",
+          unstaged = "✗",
+          staged = "✓",
+          conflict = "",
+        },
+      },
+    },
+  },
+}
