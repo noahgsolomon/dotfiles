383c1d2916430ed6aaba6b7a56366d26a85fc08e
Noah Solomon <noahsolomon2003@gmail.com>
2024-07-15 08:44:26 -0400

    other

--- init.lua
+++ init.lua
@@ -1,14 +1,13 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.g.neovide_cursor_trail_size = 0
-vim.g.neovide_show_border = true
 vim.o.guifont = "JetbrainsMono Nerd Font Mono:h14"
 vim.opt.wrap = true
 vim.opt.linebreak = true
-vim.o.background = "dark"
 
 if vim.g.neovide then
+  vim.g.neovide_cursor_trail_size = 0
+  vim.g.neovide_show_border = true
   -- Set the overall transparency
   vim.g.neovide_transparency = 0.85
   vim.g.neovide_floating_shadow = false
@@ -16,17 +15,17 @@ if vim.g.neovide then
   vim.g.neovide_floating_blur_amount_x = 2.0
   vim.g.neovide_floating_blur_amount_y = 2.0
   vim.g.neovide_window_blurred = true
-end
 
--- Set transparency for floating windows
-vim.o.pumblend = 85 -- For popup menu
-vim.o.winblend = 85 -- For floating windows
+  -- Set transparency for floating windows
+  vim.o.pumblend = 85 -- For popup menu
+  vim.o.winblend = 85 -- For floating windows
 
--- Make floating windows transparent
-vim.cmd([[
+  -- Make floating windows transparent
+  vim.cmd([[
   highlight NormalFloat guibg=NONE
   highlight Pmenu guibg=NONE
   highlight PmenuSel guibg=NONE
   highlight PmenuSbar guibg=NONE
   highlight PmenuThumb guibg=NONE
 ]])
+end
