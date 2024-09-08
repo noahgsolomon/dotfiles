be29618284593b07a98d0b47468ca591cdc32ddc
Noah Solomon <noahsolomon2003@gmail.com>
2024-09-08 11:42:19 -0400

    image support

--- init.lua
+++ init.lua
@@ -1,7 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- vim.o.guifont = "JetbrainsMono Nerd Font Mono:h14"
 vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.api.nvim_set_keymap("n", "<C-v>", "<C-q>", { noremap = true, silent = true })
